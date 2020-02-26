fc75449ff350459159a5f0b9487b3205fb0ec8ff
Estefan Hu <estefanhu074@gmail.com>
2020-02-25 23:45:15 -0800

    EOD

--- client/src/components/landing/signup.js
+++ client/src/components/landing/signup.js
@@ -18,32 +18,38 @@ const Signup = () => {
         <h1>Muse</h1>
       </div>
       <div id='form__holder'>
+        <h2>Join Project Muse</h2>
         <form onSubmit={ handleSubmit }>
           <input
+            className='signup__form--input'
             type='text'
             value={ firstName }
             onChange={ e => setFirstName(e.target.value) }
             placeholder='First Name'
           />
           <input
+            className='signup__form--input'
             type='text'
             value={ lastName }
             onChange={ e => setLastName(e.target.value) }
             placeholder='Last Name'
           />
           <input
+            className='signup__form--input'
             type='email'
             value={ email }
             onChange={ e => setEmail(e.target.value) }
             placeholder='Email Address'
           />
           <input
+            className='signup__form--input'
             type='password'
             value={ password }
             onChange={ e => setPassword(e.target.value) }
             placeholder='Password'
           />
           <input
+            className='signup__form--input'
             type='password'
             value={ confirmPassword }
             onChange={ e => setConfirmPassword(e.target.value) }
