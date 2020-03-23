2dad5dfa6e39b6a29c0632882db4b87122094988
Estefan Hu <estefanhu074@gmail.com>
2020-03-22 23:37:19 -0700

    removed blocking console.log

--- client/src/components/dashboard/items/stories.js
+++ client/src/components/dashboard/items/stories.js
@@ -9,7 +9,6 @@ const Stories = () => {
   useEffect(() => {
     fetch('http://localhost:4000/stories/' + 1)
       .then(res => res.json())
-      .then(res => console.log(res))
       .then(res => setStoriesMade(res))
       .then(console.log(storiesMade))
       .catch(console.error);
