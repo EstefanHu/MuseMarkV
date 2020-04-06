7cdcb052b1bb527a3423d226d189b208df49aad5
Estefan Hu <estefanhu074@gmail.com>
2020-04-06 00:04:27 -0700

    finishing up form setup

--- client/src/components/primary/components/create/components/write.js
+++ client/src/components/primary/components/create/components/write.js
@@ -6,9 +6,13 @@ export const Write = props => {
   const [longitude, setLongitude] = useState(props.lng);
   const [latitude, setLatitude] = useState(props.lat);
 
-  useEffect(props => {
+  useEffect(() => {
+    const modal = document.getElementById('write__modal');
     document.getElementById('write__modal--close').addEventListener('click',() => {
-      props.modal.style.display = 'none';
+      modal.style.display = 'none';
+    });
+    document.getElementById('cancelNode').addEventListener('click', () => {
+      modal.style.display = 'none';
     });
   }, [])
 
@@ -20,25 +24,46 @@ export const Write = props => {
 
   return (
     <div className='modal' id='write__modal'>
-      <div className='modal-content' id='storypoint__form'>
+      <div className='modal-content' id='storyNode__form'>
         <span className='close' id='write__modal--close'>&times;</span>
         <h1>Add Node to Story</h1>
         <form onSubmit={ handleSubmit } >
+          <label>Title for Story Node</label>
           <input
-            className='storypoint__input'
+            className='storyNode__input'
             type='text'
             value={ title }
             onChange={e => setTitle(e.target.value)}
-            placeholder='Title Node'
+            placeholder='Create a Title for this Node'
           />
+          <label>Longitude</label>
+          <input
+            className='storyNode__input'
+            type='text'
+            value={ longitude }
+            onChange={e => setLongitude(e.target.value)}
+            placeholder={ props.lng }
+          />
+          <label>Latitude</label>
+          <input
+            className='storyNode__input'
+            type='text'
+            value={ latitude }
+            onChange={e => setLatitude(e.target.value)}
+            placeholder={ props.lat }
+          />
+          <label>Content of Story Node</label>
           <textarea
-            className='storypoint__input'
+            className='storyNode__input'
             type='text'
             value={ content }
             onChange={e => setContent(e.target.value)}
             placeholder='Content'
           />
-          <input type='submit' value='Create Node' />
+          <span className='storyNode__horizontal'>
+            <input id='createNode' type='submit' value='Create Node' />
+            <button id='cancelNode'>Cancel</button>
+          </span>
         </form>
       </div>
     </div>
