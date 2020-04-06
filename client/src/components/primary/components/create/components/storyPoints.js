9c0c7fcc596be8dbf75d35e19ad69e6b3c86aee3
Estefan Hu <estefanhu074@gmail.com>
2020-04-05 20:47:22 -0700

    corrected icon positioning

--- client/src/components/primary/components/create/components/storyPoints.js
+++ client/src/components/primary/components/create/components/storyPoints.js
@@ -6,8 +6,10 @@ export const StoryPoints = props => (
   <>
   {/* TODO: Change id to title of the work */}
   {props.coords.map((point, index) => (
-    <Marker key={index} latitude={point[1]} longitude={point[0]}>
-      <MdLocationOn />
+    <Marker key={index}
+      latitude={point[1]} longitude={point[0]}
+      offsetLeft={-25} offsetTop={-48} >
+      <MdLocationOn className='map__icon' />
     </Marker>
   ))}
   </>
