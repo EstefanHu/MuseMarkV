ba1e5763ec3a3527faf42b05a72ac3c04a08b1a2
Estefan Hu <estefanhu074@gmail.com>
2020-04-14 14:20:03 -0700

    added insecure routes

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,55 +1,67 @@
 const router = require('express').Router();
 const User = require('./../models/user');
 
-router.get('/update/:id', async (req, res) => {
+router.post('/create', async (req, res) => {
   try {
-    const chosenUser = await User.findById(req.params.id);
-    res.json(chosenUser);
-  } catch(error) {    
-    res.type('text').status(500).send('Error:  ' + error);
-  }
-});
-
-router.post('/create', (req, res) => {
-  try {
-    console.log(req.body);
-    res.json('Noice');
+    let user = new User();
+    user.firstName = req.body.firstName;
+    user.lastName = req.body.lastName;
+    user.email = req.body.email;
+    user.password = req.body.password;
+    user = await user.save();
+    res.send('Successfully created user as: ' + user);
   } catch(error) {
     res.type('text').status(500).send('Error: ' + error);
   }
 });
 
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
   try {
-    console.log(req.body);
-    res.json('Logged i');
+    let user = await User.findOne({ email: req.body.email });
+    const warning = 'Email or Password was incorrect';
+    if (!user) res.send(warning);
+    if (user.password != req.password) res.send(warning);
+    res.json({user: user});
   } catch(error) {
     res.type('text').status(500).send('Error:  ' + error);
   }
 });
 
-function saveUserInformation(path) {
-  return async (req, res) => {
-    let user = req.user;
+router.get('/:id').get(async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.json(user);
+  } catch(error) {
+    res.type('text').status(500).send('Error:  ' + error);
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    let user = await User.findById(req.params.id);
     user.firstName = req.body.firstName;
     user.lastName = req.body.lastName;
     user.email = req.body.email;
     user.password = req.body.password;
-  
-    try {
-      user = await user.save();
-      res.send('Success');
-    } catch(error) {
-      res.type('text').status(500).send('Error:  ' + error);
-    }
+    user = await user.save();
+    res.send('Successfully saved user as: ' + user); //TODO: Dev only
+  } catch(error) {
+    res.type('text').status(500).send('Error:  ' + error);
   }
-}
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.send('success');
+  } catch(error) {
+    res.type('text').status(500).send('Error:  ' + error);
+  }
+});
 
 
 // Create User
 // Get User
-// Update User
 // Log User In
-// Delete User
 
 module.exports = router;
