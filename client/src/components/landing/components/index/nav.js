a79c3964ac6423d9b0c1ea2163cb69c8692e1066
Estefan Hu <estefanhu074@gmail.com>
2020-04-13 17:02:17 -0700

    added todo

--- client/src/components/landing/components/index/nav.js
+++ client/src/components/landing/components/index/nav.js
@@ -1,21 +1,26 @@
 import React from 'react';
 
+// TODO: acgtivate setBody on enter
 export const Nav = props => {
   return (
     <nav className='landing__nav--nav'>
       <ul>
         <li
           onClick={() => props.setBody('mission')}
+          tabIndex='0'
         >Mission</li>
         <li
           onClick={() => props.setBody('creator')}
+          tabIndex='0'
         >Creator</li>
         <li
           onClick={() => props.setBody('register')}
-        >Join</li>
+          tabIndex='0'
+          >Join</li>
         <li
           onClick={() => props.setBody('login')}
-        >Log in</li>
+          tabIndex='0'
+          >Log in</li>
       </ul>
     </nav>
   )
