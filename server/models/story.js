009dc473198ce001aaf653fd579e5c7a6709c7b7
Estefan Hu <estefanhu074@gmail.com>
2020-04-13 17:43:47 -0700

    migrated

--- server/models/story.js
+++ server/models/story.js
@@ -1,14 +1,26 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const storySchema = new Schema({
-  title: {
+const nodeSchema = new Schema({
+  type: {
     type: String,
-    required: true,
-    trim: true;
+    enum: ['Point'],
+    required: true
   },
-  content: {
-    trype: String,
-    require
+  coordinates: {
+    type: [Number],
+    required: true
   }
+});
+
+const turnSchema = new Schema({
+  type: {
+    type: String,
+    enum['Point'],
+    required: true
+  }
+});
+
+const storySchema = new Schema({
+  
 })
