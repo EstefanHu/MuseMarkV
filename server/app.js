2d51da5e8acf1d42619a38c8f2db800c209282d5
Estefan Hu <estefanhu074@gmail.com>
2020-04-17 14:54:01 -0700

    removed unused CORS setter

--- server/app.js
+++ server/app.js
@@ -28,12 +28,6 @@ mongoose.connection.once('open', () => {
     console.log('Connection Error: ' + err);
 });
 
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-//   });
-
 app.get('/api', (_, res) => {
     try {
         res.json(process.env.MAPBOX_ACCESS_TOKEN);
