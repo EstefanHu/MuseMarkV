a8fd63ee6159e6d01c19988336483b10f56cb04a
Estefan Hu <estefanhu074@gmail.com>
2020-04-19 15:13:11 -0700

    initialized index view

---
+++ client/src/components/landing/components/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Index = () => {
+  return (
+    <section>
+      <h1>Hello from Index</h1>
+    </section>
+  )
+}
--- client/src/components/landing/landing.js
+++ client/src/components/landing/landing.js
@@ -1,16 +1,39 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Route, Link } from 'react-router-dom';
+
 
 import './landing.css';
 
+import { Index } from './components';
 import Register from './components/register';
 import { Privacy } from './components/privacy';
 import { Terms } from './components/terms';
 
-export const Landing = () => (
-    <main id='landing'>
-        <Route exact path='/' component={Register} />
-        <Route path='/privacy' component={Privacy} />
-        <Route path='/terms' component={Terms} />
-    </main>
-)
+export const Landing = () => {
+    const [navigation, setNavigation] = useState([
+        {
+            'link': '/',
+            'name': 'Project:Muse'
+        },
+    ]);
+
+    return (
+        <>
+            <nav id='landing__nav'>
+                <ul>
+                    {navigation.map(item => (
+                        <li key={item.link}>
+                            <Link to=''>{ item.name }</Link>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+            <main id='landing__body'>
+                <Route exact path='/' component={Index} />
+                <Route path='/register' component={Register} />
+                <Route path='/privacy' component={Privacy} />
+                <Route path='/terms' component={Terms} />
+            </main>
+        </>
+    )
+}
