17161ecb0336f14e275f1a38c41262c4b9134a2c
Estefan Hu <estefanhu074@gmail.com>
2020-04-19 17:08:42 -0700

    settled background of landing index

--- client/src/components/landing/components/index.js
+++ client/src/components/landing/components/index.js
@@ -2,8 +2,17 @@ import React from 'react';
 
 export const Index = () => {
   return (
-    <section>
-      <h1>Hello from Index</h1>
+    <section id='index'>
+      <img
+        src='./img/landing.png'
+        alt='background'
+        id='index__background'
+      />
+      <div
+        id='index__content'
+      >
+        <h1>Hello from Index</h1>
+      </div>
     </section>
   )
 }
