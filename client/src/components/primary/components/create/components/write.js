56ceb800629df3608e5fa8d67881c03774f0493d
Estefan Hu <estefanhu074@gmail.com>
2020-04-22 15:25:04 -0700

    removed unused dependecy

--- client/src/components/primary/components/create/components/write.js
+++ client/src/components/primary/components/create/components/write.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 
 export const Write = props => {
   const [name, setName] = useState(props.tempNode.name);
