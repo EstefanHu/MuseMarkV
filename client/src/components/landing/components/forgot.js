9d3345b2d39bc007da88d6650bc4a49033a0b924
Estefan Hu <estefanhu074@gmail.com>
2020-04-22 23:01:46 -0700

    settled error

--- client/src/components/landing/components/forgot.js
+++ client/src/components/landing/components/forgot.js
@@ -1,11 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export const Forgot = () => {
   const [email, setEmail] = useState('');
 
+  useEffect(() => {
+    console.log(email);
+  });
+
   return (
     <section>
       <h1>I Forgot my password</h1>
+      <form>
+        <input
+          className='landing__form--input'
+          type='text'
+          value={ email }
+          onChange={e => setEmail(e.target.value)}
+          placeholder='Email Address'
+        />
+        <input type='submit' value='Submit' />
+      </form>
     </section>
   )
 }
