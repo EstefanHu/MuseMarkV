64f4c6b507c5501e620ce6024c47bfdec33d1420
Estefan Hu <estefanhu074@gmail.com>
2020-04-27 12:57:16 -0700

    added redis cache middleware

--- server/app.js
+++ server/app.js
@@ -12,44 +12,57 @@ let redisClient = redis.createClient()
 const app = express();
 
 app.use(bodyParser.json());
-app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(session({
-    secret: process.env.REDIS_KEY || 'super-secret-sessions', //TODO: Update to process.env
-    store: new RedisStore({
-        host: 'localhost',
-        port: 6379,
-        client: redisClient,
-        ttl: 260
-    }),
-    saveUninitialized: false,
-    resave: false
+  secret: process.env.REDIS_KEY || 'super-secret-sessions', //TODO: Update to process.env
+  store: new RedisStore({
+    host: 'localhost',
+    port: 6379,
+    client: redisClient,
+    ttl: 260
+  }),
+  saveUninitialized: false,
+  resave: false
 }));
 
 require('dotenv').config()
 
 const DB_CONNECTION = 'musedb';
 mongoose.connect(`mongodb://localhost/${DB_CONNECTION}`,
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false
-    });
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  });
 
 mongoose.connection.once('open', () => {
-    console.log(`connection has been established to ${DB_CONNECTION}`);
+  console.log(`connection has been established to ${DB_CONNECTION}`);
 }).on('err', err => {
-    console.log('Connection Error: ' + err);
+  console.log('Connection Error: ' + err);
 });
 
-app.get('/api', (_, res) => {
-    try {
-        res.json(process.env.MAPBOX_ACCESS_TOKEN);
-    } catch (error) {
-        res.type('text').status(500).send('Error: ' + error);
+const cache = (req, res, next) => {
+  const { id } = req.params;
+
+  redisClient.get(id, (err, data) => {
+    if (err) throw err;
+
+    if (data !== null) {
+      res.send('cached');
+    } else {
+      next();
     }
+  })
+}
+
+app.get('/api', (_, res) => {
+  try {
+    res.json(process.env.MAPBOX_ACCESS_TOKEN);
+  } catch (error) {
+    res.type('text').status(500).send('Error: ' + error);
+  }
 });
 
 const userRouter = require('./routes/user');
@@ -60,5 +73,5 @@ app.use('/story', storyRouter);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
