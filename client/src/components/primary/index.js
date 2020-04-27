6a168c93a7cf7142f17e306f76dc4b9cdd63b65e
Estefan Hu <estefanhu074@gmail.com>
2020-04-26 19:17:41 -0700

    installed tour rendering

--- client/src/components/primary/index.js
+++ client/src/components/primary/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 
+import { Tour } from './components/tour';
 import { Dashboard } from './components/dashboard';
 import { Abstract } from './components/abstract';
 import { Create } from './components/create';
@@ -18,6 +19,7 @@ export const Primary = () => {
         <StoryContext.Provider value={{ story, setStory }}>
             <Nav />
             <main>
+                <Route path='/app/tour' component={Tour} />
                 <Route path='/app/dashboard' component={Dashboard} />
                 <Route path='/app/create' component={Create} />
                 <Route path='/app/abstract/:id' component={Abstract} />
