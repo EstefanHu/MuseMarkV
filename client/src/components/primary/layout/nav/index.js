4f55fc647716ef8e369a91ce9aeba2d0db357839
Estefan Hu <estefanhu074@gmail.com>
2020-05-01 16:21:52 -0700

    prepping map merge

--- client/src/components/primary/layout/nav/index.js
+++ client/src/components/primary/layout/nav/index.js
@@ -10,18 +10,17 @@ export const Nav = () => {
 
   return (
     <nav className='primaryNav'>
-      <ul>
-        {navItems.map(item => (
-          <Link
-            key={item}
-            to={'/app/' + item.toLowerCase()}
-          >
-            <li>
-              <img src={require('./icons/' + item  + '.png')} alt='' />
-            </li>
-          </Link>
-        ))}
-      </ul>
+      {navItems.map(item => (
+        <Link
+          key={item}
+          to={'/app/' + item.toLowerCase()}
+        >
+          <img
+            src={require('./icons/' + item + '.png')}
+            alt=''
+          />
+        </Link>
+      ))}
     </nav>
   )
 }
