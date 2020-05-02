ad9570ac29810b8df7acd236120894f09d480d8b
Estefan Hu <estefanhu074@gmail.com>
2020-05-01 19:34:01 -0700

    prepairnig map migration

--- client/src/components/primary/layout/nav/index.js
+++ client/src/components/primary/layout/nav/index.js
@@ -1,26 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import {
+  MdDashboard,
+  MdSettings
+} from 'react-icons/md';
+import { RiCommunityLine } from 'react-icons/ri';
 
-export const Nav = () => {
-  const navItems = [
-    'Dashboard',
-    'Community',
-    'Settings'
-  ]
-
-  return (
-    <nav className='primaryNav'>
-      {navItems.map(item => (
-        <Link
-          key={item}
-          to={'/app/' + item.toLowerCase()}
-        >
-          <img
-            src={require('./icons/' + item + '.png')}
-            alt=''
-          />
-        </Link>
-      ))}
-    </nav>
-  )
-}
+export const Nav = () => (
+  <nav className='primaryNav'>
+    <Link to='/app/dashboard' >
+      <MdDashboard className='primaryIcon' />
+    </Link>
+    <Link to='/app/community'>
+      <RiCommunityLine className='primaryIcon' />
+    </Link>
+    <Link to='/app/settings'>
+      <MdSettings className='primaryIcon' />
+    </Link>
+  </nav>
+)
