be82b92e4ded053e0a59b29ce38e6119c9d8376f
Estefan Hu <estefanhu074@gmail.com>
2020-05-04 15:39:51 -0700

    removed home route

--- client/src/components/primary/layout/nav.js
+++ client/src/components/primary/layout/nav.js
@@ -2,18 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import {
   MdHome,
-  MdDashboard,
   MdLayers,
   MdSettings
 } from 'react-icons/md';
 
 export const Nav = () => (
   <nav className='primaryNav'>
-    <Link to='/app/home' >
-      <MdHome className='primaryIcon' />
-    </Link>
     <Link to='/app/dashboard' >
-      <MdDashboard className='primaryIcon' />
+      <MdHome className='primaryIcon' />
     </Link>
     <Link to='/app/community'>
       <MdLayers className='primaryIcon' />
