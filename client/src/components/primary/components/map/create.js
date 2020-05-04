5e8cb6cbf7d91ab23dc2b297091b160f573b07e6
Estefan Hu <estefanhu074@gmail.com>
2020-05-03 21:11:11 -0700

    reformatted

--- client/src/components/primary/components/map/create.js
+++ client/src/components/primary/components/map/create.js
@@ -16,60 +16,61 @@ export const Create = props => {
       {story.route.map((node, index) => (
         <>
           {node.type === 'node' ? (
-          <Marker
-            key={node.position}
-            latitude={node.coordinates[1]}
-            longitude={node.coordinates[0]}
-            offsetLeft={-25}
-            offsetTop={-47}
-          >
-            <MdLocationOn
-              className='map__icon'
-              onClick={() => props.engageNode(node.position)}
-            />
-          </Marker>
-        ) : (
-          <Marker
-            key={node.position}
-            latitude={node.coordinates[1]}
-            longitude={node.coordinates[0]}
-            offsetLeft={-6.5}
-            offsetTop={-8}
-          >
-            <FaDotCircle
-              className='map__icon--turn'
-              onClick={() => props.engageNode(node.position)}
-            />
-          </Marker>
-          )}
+            <Marker
+              key={node.position}
+              latitude={node.coordinates[1]}
+              longitude={node.coordinates[0]}
+              offsetLeft={-25}
+              offsetTop={-47}
+            >
+              <MdLocationOn
+                className='map__icon'
+                onClick={() => props.engageNode(node.position)}
+              />
+            </Marker>
+          ) : (
+              <Marker
+                key={node.position}
+                latitude={node.coordinates[1]}
+                longitude={node.coordinates[0]}
+                offsetLeft={-6.5}
+                offsetTop={-8}
+              >
+                <FaDotCircle
+                  className='map__icon--turn'
+                  onClick={() => props.engageNode(node.position)}
+                />
+              </Marker>
+            )
+          }
 
           {story.route[index + 1] &&
-          <Source type='geojson'
-            key={node.position}
-            data={{
-              "type": "FeatureCollection",
-              "features": [
-                {
-                  "type": "Feature",
-                  "geometry": {
-                    "type": "LineString",
-                    "coordinates": [
-                      node.coordinates,
-                      story.route[index + 1].coordinates
-                    ]
+            <Source type='geojson'
+              key={node.position}
+              data={{
+                "type": "FeatureCollection",
+                "features": [
+                  {
+                    "type": "Feature",
+                    "geometry": {
+                      "type": "LineString",
+                      "coordinates": [
+                        node.coordinates,
+                        story.route[index + 1].coordinates
+                      ]
+                    }
                   }
-                }
-              ]
-            }}>
-            <Layer
-              type='line'
-              paint={{
-                'line-width': 3,
-                'line-color': '#007cbf',
-
-              }}
-            />
-          </Source>}
+                ]
+              }}>
+              <Layer
+                type='line'
+                paint={{
+                  'line-width': 3,
+                  'line-color': '#007cbf',
+                }}
+              />
+            </Source>
+          }
         </>
       ))}
     </>
