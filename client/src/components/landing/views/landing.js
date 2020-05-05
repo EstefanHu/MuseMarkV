d036c879f0cfec31aa0204fa96a9f95d7cb2d5ab
Estefan Hu <estefanhu074@gmail.com>
2020-05-05 16:17:42 -0700

    added welcome text

--- client/src/components/landing/views/landing.js
+++ client/src/components/landing/views/landing.js
@@ -6,6 +6,8 @@ export const Index = () => {
       <h1>Project<br />:Muse</h1>
       <section className='landingContent'>
         <h2>Welcome to the Muse Project</h2>
+        <p>Project:Muse is an experimental story telling platform <br />
+          that accentuates the world around us.</p>
       </section>
     </section>
   )
