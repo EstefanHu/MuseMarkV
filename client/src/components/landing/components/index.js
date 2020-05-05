435beec0f36726da7c8ca2dafa75c84577636ba4
Estefan Hu <estefanhu074@gmail.com>
2020-05-05 00:42:24 -0700

    installed loading spinner

--- client/src/components/landing/components/index.js
+++ client/src/components/landing/components/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import { Loading } from '../../primary/layout/loading';
+
 export const Index = () => {
   
   return (
     <section id='container'>
-      
+      <Loading />
     </section>
   )
 }
