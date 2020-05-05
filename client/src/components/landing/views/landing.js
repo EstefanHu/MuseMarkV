ed6654a7b3a8a61257af8b87675c85a5e011ecd0
Estefan Hu <estefanhu074@gmail.com>
2020-05-05 15:49:50 -0700

    testing on laptop

--- client/src/components/landing/views/landing.js
+++ client/src/components/landing/views/landing.js
@@ -4,6 +4,9 @@ export const Index = () => {
   return (
     <section id='landingContainer'>
       <h1>Project<br />:Muse</h1>
+      <section className='landingContent'>
+        <h2>Welcome to the Muse Project</h2>
+      </section>
     </section>
   )
 }
