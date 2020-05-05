f545225f72bdc2ef1f8d42d58a7330b262511fad
Estefan Hu <estefanhu074@gmail.com>
2020-05-05 00:58:37 -0700

    removed loading from index

--- client/src/components/landing/components/index.js
+++ client/src/components/landing/components/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
 
-import { Loading } from '../../primary/layout/loading';
-
 export const Index = () => {
-  
   return (
     <section id='container'>
-      <Loading />
     </section>
   )
 }
