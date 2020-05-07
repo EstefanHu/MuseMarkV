babf450645a2da4cf15a10926fe15883ac71b322
Estefan Hu <estefanhu074@gmail.com>
2020-05-06 19:21:34 -0700

    testing rendering

--- client/src/primary/components/settings/profile.js
+++ client/src/primary/components/settings/profile.js
@@ -1,11 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export const Profile = ({firstName, lastName, email}) => (
-  <section
-    className='settings__profile'
-  >
-    <h1>{firstName} {lastName}</h1>
-    <h2>{email}</h2>
-    
-  </section>
-)
+export const Profile = (props) => {
+  const [firstName, setFirstName] = useState(props.firstName);
+  const [lastName, setLastName] = useState(props.lastName);
+  const [email, setEmail] = useState(props.email);
+  const [isUpdating, setIsUpdating] = useState(false);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    // fetch('http://localhost:4000/user/updated', {
+    //   credentials: 'include',
+    //   method: 'POST',
+    //   headers: {
+    //     Accept: 'application/json',
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify({
+    //     firstName,
+    //     lastName,
+    //     email
+    //   })
+    // })
+    //   .then(res => res.json())
+    //   .then(res => {
+    //     if (res.error) alert(res.error);
+    //   })
+    //   .catch(console.error);
+
+    setIsUpdating(isUpdating => !isUpdating);
+  }
+
+  return isUpdating ? (
+    <form onSubmit={handleSubmit}>
+      <input
+        className='settings__update-input'
+        type='text'
+        value={firstName}
+        onChange={e => setFirstName(e.target.value)}
+        placeholder='First Name'
+        required
+      />
+      <input
+        className='settings__update-input'
+        type='text'
+        value={lastName}
+        onChange={e => setLastName(e.target.value)}
+        placeholder='Last Name'
+        required
+      />
+      <input
+        className='settings__update-input'
+        type='email'
+        value='email'
+        onChange={e => setEmail(e.target.value)}
+        placeholder='Email Address'
+        required
+      />
+
+      <input type='submit' value='Update' />
+    </form>
+  ) : (
+      <section
+        className='settings__profile'
+      >
+        <h1>{props.firstName} {props.lastName}</h1>
+        <h2>{props.email}</h2>
+      <button
+      onClick={() => setIsUpdating(isUpdating => !isUpdating)}
+      >Update</button>
+      </section>
+    )
+}
